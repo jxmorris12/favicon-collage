14ce6d545513529105d71f1f579b93441dfca7d3
Jack Morris <jxmorris12@gmail.com>
2017-06-13 00:31:19 -0700

    more local favico, nice flexbox action, cool printing, some other bug fixes

--- front-end/favico-react.js
+++ front-end/favico-react.js
@@ -1,28 +1,83 @@
 // jack morris 06/12/17
 
+/*
+ * FavicoApp class
+ */
+class FavicoApp extends React.Component {
+  constructor(props) {
+    super();
+    this.sites = props.sites;
+  }
+  render() {
+    console.log(this.sites);
+    let children = this.sites.map(site => { return React.createElement(FavicoBox, site); });
+    return React.createElement('div', {
+      'className': 'favico-container'
+    }, 
+    children);
+  }
+}
 
 /*
  * FavicoBox class
  */
 class FavicoBox extends React.Component {
   constructor(props) {
-    super(props);
+    super();
+    this.name = props.name;
+    this.tld = props.tld;
+    this.hue = props.hue;
+
+  }
+  getImageUrl() {
+    if(this.hue >= 0) {
+      // we have an image
+      return 'http://' + this.name + '.' + this.tld + '/favicon.ico';
+    } else {
+      // get placeholder
+      return placeholderUrl;
+    }
   }
   render() {
+    let innerImage = React.createElement(FavicoImage, { 'url': this.getImageUrl() });
     return React.createElement(
       "div",
       {
-        'class': 'favico-box'
+        'className': 'favico-box',
+        'id': this.props.name
       },
-      "Hello ",
-      this.props.name
+      null,
+      innerImage
     );
   }
 }
 
+/*
+ * FavicoImage class
+ */
+class FavicoImage extends React.Component {
+  constructor(props) {
+    super();
+    this.url = props.url;
+  }
+  render() {
+    return React.createElement(
+      'img',
+      {
+        'src': this.url,
+        'className': 'favico-image'
+      }
+    );
+  }
+}
+
+/*
+ * Helper methods
+ */
 
 /*
  * Main initialization code
  */
 let container = document.getElementById('container');
-ReactDOM.render(React.createElement(FavicoBox, { name: "John" }), container);
+let reactApp = React.createElement(FavicoApp, { 'sites': topSites });
+ReactDOM.render(reactApp, container);
